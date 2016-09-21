c634ad5f867fbdf184cd005ff693a712459d214c
刘傲 <359866091@qq.com>
2016-09-21 09:51:22 +0800

    推送页面里的ListView中的 三个图的布局

--- app/src/main/java/com/lanou3g/carhome/recommend/RecommendAdapter.java
+++ app/src/main/java/com/lanou3g/carhome/recommend/RecommendAdapter.java
@@ -19,6 +19,9 @@ public class RecommendAdapter extends BaseAdapter {
 
     Context context;
     RecommendBean bean;
+    private final int TYPE_COUNT = 2;
+    private final int TYPE_NORMAL = 0;
+    private final int TYPE_THREE_IMAGE = 1;
 
     public RecommendAdapter(Context context) {
         this.context = context;
@@ -46,27 +49,73 @@ public class RecommendAdapter extends BaseAdapter {
     }
 
     @Override
+    public int getViewTypeCount() {
+        return TYPE_COUNT;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (bean.getResult().getNewslist().get(position).getMediatype() == 6){
+            return TYPE_THREE_IMAGE;
+        } else {
+            return TYPE_NORMAL;
+        }
+    }
+
+    @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
         RecommendViewHolder viewHolder = null;
+        RecommendViewHolder1 viewHolder1 = null;
+        int type = getItemViewType(i);
         if (view == null) {
-            view = LayoutInflater.from(context).inflate(R.layout.item_recommend, null);
-            viewHolder = new RecommendViewHolder(view);
-            view.setTag(viewHolder);
+            if (type == TYPE_THREE_IMAGE) {
+                view = LayoutInflater.from(context).inflate(R.layout.item_recommend_img, null);
+                viewHolder1 = new RecommendViewHolder1(view);
+                view.setTag(viewHolder1);
+            } else if (type == TYPE_NORMAL){
+                view = LayoutInflater.from(context).inflate(R.layout.item_recommend, null);
+                viewHolder = new RecommendViewHolder(view);
+                view.setTag(viewHolder);
+            }
+
         } else {
-            viewHolder = (RecommendViewHolder) view.getTag();
+            if (type == TYPE_THREE_IMAGE) {
+                viewHolder1 = (RecommendViewHolder1) view.getTag();
+            } else if (type == TYPE_NORMAL){
+                viewHolder = (RecommendViewHolder) view.getTag();
+            }
         }
 
-        viewHolder.tvTitle.setText(bean.getResult().getNewslist().get(i).getTitle());
-        viewHolder.tvDate.setText(bean.getResult().getNewslist().get(i).getTime());
-        if (!(bean.getResult().getNewslist().get(i).getSmallpic().equals(""))) {
-            Picasso.with(context).load(bean.getResult().getNewslist().get(i).getSmallpic()).into(viewHolder.ivImage);
-        }
-        if (bean.getResult().getNewslist().get(i).getMediatype() == 3) {
-            viewHolder.tvComment.setText(bean.getResult().getNewslist().get(i).getReplycount() + "播放");
-        } else if (bean.getResult().getNewslist().get(i).getMediatype() == 2) {
-            viewHolder.tvComment.setText(bean.getResult().getNewslist().get(i).getReplycount() + "评论");
-        } else if (bean.getResult().getNewslist().get(i).getMediatype() == 1) {
-            viewHolder.tvComment.setText(bean.getResult().getNewslist().get(i).getReplycount() + "评论");
+        if (type == TYPE_THREE_IMAGE){
+
+            String url = bean.getResult().getNewslist().get(i).getIndexdetail();
+            String[] args = url.split("㊣");
+            String[] args1 = args[2].split(",");
+            String one = args1[0];
+            String two = args1[1];
+            String three = args1[2];
+
+            Picasso.with(context).load(one).into(viewHolder1.imgIvImageOne);
+            Picasso.with(context).load(two).into(viewHolder1.imgIvImageTwo);
+            Picasso.with(context).load(three).into(viewHolder1.imgIvImageThree);
+
+            viewHolder1.imgTvTitle.setText(bean.getResult().getNewslist().get(i).getTitle());
+            viewHolder1.imgTvComment.setText(bean.getResult().getNewslist().get(i).getReplycount() + "图片");
+            viewHolder1.imgTvDate.setText(bean.getResult().getNewslist().get(i).getTime());
+
+        } else if (type == TYPE_NORMAL){
+            viewHolder.tvTitle.setText(bean.getResult().getNewslist().get(i).getTitle());
+            viewHolder.tvDate.setText(bean.getResult().getNewslist().get(i).getTime());
+            if (!(bean.getResult().getNewslist().get(i).getSmallpic().equals(""))) {
+                Picasso.with(context).load(bean.getResult().getNewslist().get(i).getSmallpic()).into(viewHolder.ivImage);
+            }
+            if (bean.getResult().getNewslist().get(i).getMediatype() == 3) {
+                viewHolder.tvComment.setText(bean.getResult().getNewslist().get(i).getReplycount() + "播放");
+            } else if (bean.getResult().getNewslist().get(i).getMediatype() == 2) {
+                viewHolder.tvComment.setText(bean.getResult().getNewslist().get(i).getReplycount() + "评论");
+            } else if (bean.getResult().getNewslist().get(i).getMediatype() == 1) {
+                viewHolder.tvComment.setText(bean.getResult().getNewslist().get(i).getReplycount() + "评论");
+            }
         }
         return view;
     }
@@ -87,4 +136,23 @@ public class RecommendAdapter extends BaseAdapter {
         }
     }
 
+    private class RecommendViewHolder1 {
+
+        private final ImageView imgIvImageOne;
+        private final ImageView imgIvImageTwo;
+        private final ImageView imgIvImageThree;
+        private final TextView imgTvDate;
+        private final TextView imgTvComment;
+        private final TextView imgTvTitle;
+
+        public RecommendViewHolder1(View view) {
+
+            imgIvImageOne = (ImageView) view.findViewById(R.id.item_recommend_img_image_one);
+            imgIvImageTwo = (ImageView) view.findViewById(R.id.item_recommend_img_image_two);
+            imgIvImageThree = (ImageView) view.findViewById(R.id.item_recommend_img_image_three);
+            imgTvDate = (TextView) view.findViewById(R.id.item_recommend_img_date);
+            imgTvComment = (TextView) view.findViewById(R.id.item_recommend_img_comment);
+            imgTvTitle = (TextView) view.findViewById(R.id.item_recommend_img_title);
+        }
+    }
 }
