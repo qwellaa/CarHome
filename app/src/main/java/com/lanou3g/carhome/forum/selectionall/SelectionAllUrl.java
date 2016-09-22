6d4a0046d6434e1ef0e6391634cd8e366baf74cd
刘傲 <359866091@qq.com>
2016-09-22 21:08:10 +0800

    第四天

---
+++ app/src/main/java/com/lanou3g/carhome/forum/selectionall/SelectionAllUrl.java
@@ -0,0 +1,60 @@
+package com.lanou3g.carhome.forum.selectionall;
+
+import com.lanou3g.carhome.networkrequest.URLValues;
+
+import java.util.ArrayList;
+
+/**
+ *
+ */
+public class SelectionAllUrl {
+
+    private ArrayList<String> urlList = new ArrayList<>();
+
+
+    public ArrayList<String> getUrlList() {
+        urlList.add(URLValues.WIFE_URL);
+        urlList.add(URLValues.BEAUTY_URL);
+        urlList.add(URLValues.RED_PEOPLE_URL);
+        urlList.add(URLValues.FORUM_LECTURER_URL);
+        urlList.add(URLValues.CHOSECARE_URL);
+        urlList.add(URLValues.ADVISE_OTHERS_URL);
+        urlList.add(URLValues.HIGH_URL);
+        urlList.add(URLValues.ELECTRIC_CARS_URL);
+        urlList.add(URLValues.SEND_CAR_URL);
+        urlList.add(URLValues.DRIVIND_CRITIQUE_URL);
+        urlList.add(URLValues.DRIVING_MEMBER_URL);
+        urlList.add(URLValues.OVERSEAS_BUYERS_URL);
+        urlList.add(URLValues.CLASSIC_CAR_URL);
+        urlList.add(URLValues.GIRL_CHOSE_URL);
+        urlList.add(URLValues.PREFERENTIAL_CAR_URL);
+        urlList.add(URLValues.ORIGINAL_LARGE_URL);
+        urlList.add(URLValues.TOP_ELEGANT_DEMEANOUR_URL);
+        urlList.add(URLValues.CONVERSION_URL);
+        urlList.add(URLValues.WAY_OF_MOTORING_URL);
+        urlList.add(URLValues.FIRST_CAMP_URL);
+        urlList.add(URLValues.NEW_LIVE_URL);
+        urlList.add(URLValues.HISTORICAL_TOPIC_URL);
+        urlList.add(URLValues.GREAT_VIDEO_URL);
+        urlList.add(URLValues.MO_URL);
+        urlList.add(URLValues.HONEYMOON_URL);
+        urlList.add(URLValues.SWEET_WEDDING_URL);
+        urlList.add(URLValues.PHOTOGRAPHY_CLASS_URL);
+        urlList.add(URLValues.MY_PARTY_URL);
+        urlList.add(URLValues.BIKE_TRIBE_URL);
+        urlList.add(URLValues.GOSSIP_CLUB_URL);
+        urlList.add(URLValues.NORTH_CHINA_TRAVEL_URL);
+        urlList.add(URLValues.SOUTHWEST_CHINA_TRAVEL_URL);
+        urlList.add(URLValues.NORTHEAST_CHINA_TRAVEL_URL);
+        urlList.add(URLValues.NORTHWEST_CHINA_TRAVEL_URL);
+        urlList.add(URLValues.CENTRAL_CHINA_TRAVEL_URL);
+        urlList.add(URLValues.SOUTH_CHINA_TRAVEL_URL);
+        urlList.add(URLValues.EAST_CHINA_TRAVEL_URL);
+        urlList.add(URLValues.HK_MACAO_TW_TRAVEL_URL);
+        urlList.add(URLValues.OVERSEAS_TRAVEL_URL);
+        urlList.add(URLValues.SEA_PEARL_URL);
+        return urlList;
+    }
+
+
+}
--- app/src/main/java/com/lanou3g/carhome/networkrequest/URLValues.java
+++ app/src/main/java/com/lanou3g/carhome/networkrequest/URLValues.java
@@ -12,24 +12,90 @@ public final class URLValues {
     public static final String NEW_URL="http://app.api.autohome.com.cn/autov4.8.8/news/newslist-pm1-c0-nt0-p1-s30-l0.json";
 
     // 第二页 论坛
+
+    public static final String SELECTION_URL = "http://223.99.255.20/clubnc.app.autohome.com.cn/club_v7.0.5/club/jingxuantopic.ashx?platud=2&categoryid=0&pageindex=1&pagesize=30&devicetype=android.1501_M02&deviceid=860954030358581&userid=0&operation=1&netstate=0&gps=38.889726%2C121.550943";
     // 精选
-    public static final String SELECTION_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c0-p1-s30.json";
+//    public static final String SELECTION_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c0-p1-s30.json";
     // 24小时热帖
     public static final String TFHOURS_URL = "http://183.232.160.141/club.app.autohome.com.cn/club_v7.0.5/club/shotfoumlist-pm2-p1-s50.json";
     // 媳妇当车模
     public static final String WIFE_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c104-p1-s30.json";
     // 美人'记'
-    public static final String BEAUTY_URL ="http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c110-p1-s30.json";
+    public static final String BEAUTY_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c110-p1-s30.json";
+    // 论坛红人馆
+    public static final String RED_PEOPLE_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c172-p1-s30.json";
+    // 论坛讲师
+    public static final String FORUM_LECTURER_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c230-p1-s30.json";
+    // 精挑细选
+    public static final String CHOSECARE_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c121-p1-s30.json";
+    // 现身说法
+    public static final String ADVISE_OTHERS_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c106-p1-s30.json";
     // 高端阵地
     public static final String HIGH_URL ="http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c118-p1-s30.json";
-    // 论坛红人馆
-    public static final String RED_PEOPLE_URL ="http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c172-p1-s30.json";
-    // 摩友天地
-    public static final String MO_URL ="http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c184-p1-s30.json";
-    // 精挑细选
-    public static final String CHOSECARE_URL ="http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c121-p1-s30.json";
+    // 电动车
+    public static final String ELECTRIC_CARS_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c210-p1-s30.json";
+    // 汇买车
+    public static final String SEND_CAR_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c199-p1-s30.json";
+    // 行车点评
+    public static final String DRIVIND_CRITIQUE_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c198-p1-s30.json";
+    // 超级试驾员
+    public static final String DRIVING_MEMBER_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c168-p1-s30.json";
+    // 海外购车
+    public static final String OVERSEAS_BUYERS_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c113-p1-s30.json";
+    // 经典老车
+    public static final String CLASSIC_CAR_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c109-p1-s30.json";
     // 妹子选车
     public static final String GIRL_CHOSE_URL ="http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c191-p1-s30.json";
+    // 优惠购车
+    public static final String PREFERENTIAL_CAR_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c196-p1-s30.json";
+    // 原创大片
+    public static final String ORIGINAL_LARGE_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c107-p1-s30.json";
+    // 顶配风采
+    public static final String TOP_ELEGANT_DEMEANOUR_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c105-p1-s30.json";
     // 改装有理
     public static final String CONVERSION_URL ="http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c122-p1-s30.json";
+    // 养车之道
+    public static final String WAY_OF_MOTORING_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c194-p1-s30.json";
+    // 首发阵营
+    public static final String FIRST_CAMP_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c119-p1-s30.json";
+    // 新车直播
+    public static final String NEW_LIVE_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c112-p1-s30.json";
+    // 历史选题
+    public static final String HISTORICAL_TOPIC_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c120-p1-s30.json";
+    // 精彩视频
+    public static final String GREAT_VIDEO_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c227-p1-s30.json";
+    // 摩友天地
+    public static final String MO_URL ="http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c184-p1-s30.json";
+    // 蜜月之旅
+    public static final String HONEYMOON_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c108-p1-s30.json";
+    // 甜蜜婚礼
+    public static final String SWEET_WEDDING_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c124-p1-s30.json";
+    // 摄影课堂
+    public static final String PHOTOGRAPHY_CLASS_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c123-p1-s30.json";
+    // 车友聚会
+    public static final String MY_PARTY_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c185-p1-s30.json";
+    // 单车部落
+    public static final String BIKE_TRIBE_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c186-p1-s30.json";
+    // 杂谈俱乐部
+    public static final String GOSSIP_CLUB_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c214-p1-s30.json";
+    // 华北游记
+    public static final String NORTH_CHINA_TRAVEL_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c218-p1-s30.json";
+    // 西南游记
+    public static final String SOUTHWEST_CHINA_TRAVEL_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c223-p1-s30.json";
+    // 东北游记
+    public static final String NORTHEAST_CHINA_TRAVEL_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c221-p1-s30.json";
+    // 西北游记 :
+    public static final String NORTHWEST_CHINA_TRAVEL_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c222-p1-s30.json";
+    // 华中游记 :
+    public static final String CENTRAL_CHINA_TRAVEL_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c220-p1-s30.json";
+    // 华南游记 :
+    public static final String SOUTH_CHINA_TRAVEL_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c224-p1-s30.json";
+    // 华东游记 :
+    public static final String EAST_CHINA_TRAVEL_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c219-p1-s30.json";
+    // 港澳台游记 :
+    public static final String HK_MACAO_TW_TRAVEL_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c225-p1-s30.json";
+    // 海外游记 :
+    public static final String OVERSEAS_TRAVEL_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c226-p1-s30.json";
+    // 沧海遗珠 :
+    public static final String SEA_PEARL_URL = "http://app.api.autohome.com.cn/autov4.8.8/club/jingxuantopic-pm1-c212-p1-s30.json";
 }
