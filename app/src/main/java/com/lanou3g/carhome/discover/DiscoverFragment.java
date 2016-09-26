0e72b394a6043ad574218ecb0bad9cf90a668672
刘傲 <359866091@qq.com>
2016-09-26 20:16:20 +0800

    发现的一级界面已经完成

--- app/src/main/java/com/lanou3g/carhome/discover/DiscoverFragment.java
+++ app/src/main/java/com/lanou3g/carhome/discover/DiscoverFragment.java
@@ -18,12 +18,17 @@ import com.handmark.pulltorefresh.library.PullToRefreshListView;
 import com.lanou3g.carhome.R;
 import com.lanou3g.carhome.baseclass.BaseFragment;
 import com.lanou3g.carhome.discover.activityzone.ActivityZoneAdapter;
+import com.lanou3g.carhome.discover.businessenty.BusinessAdapter;
 import com.lanou3g.carhome.discover.formerecommend.ForMeRecommedAdapter;
 import com.lanou3g.carhome.discover.goodslist.GoodsListAdapter;
 import com.lanou3g.carhome.networkrequest.GsonRequest;
 import com.lanou3g.carhome.networkrequest.URLValues;
 import com.lanou3g.carhome.networkrequest.VolleySingleton;
 import com.squareup.picasso.Picasso;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+
+import java.util.ArrayList;
 
 /**
  *
@@ -49,6 +54,13 @@ public class DiscoverFragment extends BaseFragment{
     private RecyclerView mServiceZoneRv;
     private TextView mTVtitleServicrZone;
     private ActivityZoneAdapter mServiceZoneAdapter;
+    private ImageView mIvTextScoll;
+    private TextView mTvTextScoll;
+    private TextView mTvTitleScoll;
+    private RecyclerView mRvEntry;
+    private BusinessAdapter businessAdapter;
+    private ImageView ivBigImage;
+    private Banner banner;
 
     @Override
     protected int setLayout() {
@@ -92,6 +104,8 @@ public class DiscoverFragment extends BaseFragment{
         zoneAdapter = new ActivityZoneAdapter(context);
         mSaleAdapter = new ActivityZoneAdapter(context);
         mServiceZoneAdapter = new ActivityZoneAdapter(context);
+        businessAdapter = new BusinessAdapter(context);
+
 
         plvDiscover.setAdapter(goodsListAdapter);
         rvForMe.setAdapter(forMeRecommedAdapter);
@@ -99,6 +113,8 @@ public class DiscoverFragment extends BaseFragment{
         rvZone.setAdapter(zoneAdapter);
         mRvSale.setAdapter(mSaleAdapter);
         mServiceZoneRv.setAdapter(mServiceZoneAdapter);
+        mRvEntry.setAdapter(businessAdapter);
+
 
         GridLayoutManager forMeManager = new GridLayoutManager(context, 2);
         rvForMe.setLayoutManager(forMeManager);
@@ -106,13 +122,13 @@ public class DiscoverFragment extends BaseFragment{
         rvGuess.setLayoutManager(guessMeManager);
         GridLayoutManager zoneManager = new GridLayoutManager(context, 3);
         rvZone.setLayoutManager(zoneManager);
-
         LinearLayoutManager saleManager = new LinearLayoutManager(context);
         saleManager.setOrientation(LinearLayoutManager.HORIZONTAL);
         mRvSale.setLayoutManager(saleManager);
-
         GridLayoutManager serviceZoneManager = new GridLayoutManager(context, 2);
         mServiceZoneRv.setLayoutManager(serviceZoneManager);
+        GridLayoutManager entryManager = new GridLayoutManager(context, 5);
+        mRvEntry.setLayoutManager(entryManager);
 
     }
 
@@ -179,7 +195,33 @@ public class DiscoverFragment extends BaseFragment{
             if (response.getResult().getCardlist().get(i).getDescription().equals("田字小号专区")) {
                 mServiceZoneAdapter.setId(i);
                 mServiceZoneAdapter.setBean(response);
+                mTvTitleScoll.setText(response.getResult().getCardlist().get(i).getTitle());
             }
+
+            if (response.getResult().getCardlist().get(i).getDescription().equals("文字滚动链")) {
+                Picasso.with(context).load(response.getResult().getCardlist().get(i).getImageurl()).into(mIvTextScoll);
+            }
+
+            if (response.getResult().getCardlist().get(i).getDescription().equals("业务入口")) {
+                businessAdapter.setId(i);
+                businessAdapter.setBean(response);
+            }
+
+            if (response.getResult().getCardlist().get(i).getDescription().equals("单帧大号横栏")) {
+                Picasso.with(context).load(response.getResult().getCardlist().get(i).getData().get(0).getImageurl()).into(ivBigImage);
+            }
+
+            if (response.getResult().getCardlist().get(i).getDescription().equals("焦点图")) {
+                ArrayList<String> images = new ArrayList<>();
+                for (int j = 0; j < response.getResult().getCardlist().get(i).getData().size(); j++) {
+                   images.add(response.getResult().getCardlist().get(i).getData().get(j).getImageurl());
+                }
+
+                banner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR);
+                banner.setIndicatorGravity(BannerConfig.RIGHT);
+                banner.setImages(images);
+            }
+
         }
     }
 
@@ -209,7 +251,26 @@ public class DiscoverFragment extends BaseFragment{
         mServiceZoneRv = (RecyclerView) viewServiceZone.findViewById(R.id.rv_headview_service_zone);
         mTVtitleServicrZone = (TextView) viewServiceZone.findViewById(R.id.tv_heandview_title_service_zone);
 
+        View viewTextScoll = LayoutInflater.from(context).inflate(R.layout.headview_text_scoll, null);
+        mIvTextScoll = (ImageView) viewTextScoll.findViewById(R.id.iv_headview_text_scoll);
+        mTvTextScoll = (TextView) viewTextScoll.findViewById(R.id.tv_headview_body_text_scoll);
+        mTvTitleScoll = (TextView) viewTextScoll.findViewById(R.id.tv_headview_title_text_scoll);
+
+        View viewBusinessEntry = LayoutInflater.from(context).inflate(R.layout.headview_business_entry, null);
+        mRvEntry = (RecyclerView) viewBusinessEntry.findViewById(R.id.rv_headview_business_entry);
+
+        View viewBigImage = LayoutInflater.from(context).inflate(R.layout.headview_big_image, null);
+        ivBigImage = (ImageView) viewBigImage.findViewById(R.id.iv_headview_big_image);
+
+        View viewBinner = LayoutInflater.from(context).inflate(R.layout.headview_banner, null);
+        banner = new Banner(context);
+        banner = (Banner) viewBinner.findViewById(R.id.banner_heandview_banner);
+
         ListView lv = plvDiscover.getRefreshableView();
+        lv.addHeaderView(viewBinner);
+        lv.addHeaderView(viewBigImage);
+        lv.addHeaderView(viewBusinessEntry);
+        lv.addHeaderView(viewTextScoll);
         lv.addHeaderView(viewServiceZone);
         lv.addHeaderView(viewFlashSale);
         lv.addHeaderView(viewActivityZone);
---
+++ app/src/main/java/com/lanou3g/carhome/discover/businessenty/BusinessAdapter.java
@@ -0,0 +1,66 @@
+package com.lanou3g.carhome.discover.businessenty;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.lanou3g.carhome.R;
+import com.lanou3g.carhome.discover.DiscoverBean;
+import com.squareup.picasso.Picasso;
+
+/**
+ *
+ */
+public class BusinessAdapter extends RecyclerView.Adapter<BusinessAdapter.ViewHolder>{
+
+    private Context context;
+    private DiscoverBean bean;
+    private int id;
+
+    public BusinessAdapter(Context context) {
+        this.context = context;
+    }
+
+    public void setBean(DiscoverBean bean) {
+        this.bean = bean;
+        notifyDataSetChanged();
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_discover_business_entry, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.tv.setText(bean.getResult().getCardlist().get(id).getData().get(position).getTitle());
+        Picasso.with(context).load(bean.getResult().getCardlist().get(id).getData().get(position).getImageurl()).into(holder.image);
+    }
+
+    @Override
+    public int getItemCount() {
+        return bean == null ? 0 : bean.getResult().getCardlist().get(id).getData().size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView tv;
+        private final ImageView image;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            tv = (TextView) itemView.findViewById(R.id.tv_item_business_entry);
+            image = (ImageView) itemView.findViewById(R.id.iv_item_business_entry);
+        }
+    }
+}
