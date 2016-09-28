58379e3e695fa5092de41cc81180e0fa9bf70ee0
刘傲 <359866091@qq.com>
2016-09-28 20:45:38 +0800

    第三页 二手车 webview

--- app/src/main/java/com/lanou3g/carhome/findcar/usedcar/UsedCarFragment.java
+++ app/src/main/java/com/lanou3g/carhome/findcar/usedcar/UsedCarFragment.java
@@ -1,12 +1,20 @@
 package com.lanou3g.carhome.findcar.usedcar;
 
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 import com.lanou3g.carhome.R;
 import com.lanou3g.carhome.baseclass.BaseFragment;
+import com.lanou3g.carhome.networkrequest.URLValues;
 
 /**
  *
  */
 public class UsedCarFragment extends BaseFragment{
+
+    private WebView wv;
+
     @Override
     protected int setLayout() {
         return R.layout.fragment_findcar_used_car;
@@ -14,11 +22,20 @@ public class UsedCarFragment extends BaseFragment{
 
     @Override
     protected void initView() {
-
+        wv = bindView(R.id.wv_use_car);
     }
 
     @Override
     protected void initData() {
-
+        wv.loadUrl(URLValues.USE_CAR_URL);
+        wv.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        WebSettings settings = wv.getSettings();
+        settings.setJavaScriptEnabled(true);
     }
 }
